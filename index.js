9ebe39f205dc7547d12d5e6efa6cfffa6165fc7f
unknown <isaruhullah035@gmail.com>
2025-11-11 18:07:24 +0600

    mathod added

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// server.js
+
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config(); // ⬅️ load .env variables
+require('dotenv').config(); // 
 
 const app = express();
 const port = process.env.PORT || 5000;
